38e4562770f954c16362c7a2669ffd5bd80991ad
Bernardo Vargas <bernardo.vargasf@hotmail.com>
2020-10-25 22:38:15 -0600

    Making the project a standalone

--- src/main/java/com/varflober/CountryApplication.java
+++ src/main/java/com/varflober/CountryApplication.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.varflober" })
 public class CountryApplication {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
